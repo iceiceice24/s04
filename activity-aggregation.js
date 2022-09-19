43b1a61f0d721509709048d9e51e9cf3d0efb77a
iceiceice24 <82870335+iceiceice24@users.noreply.github.com>
2022-09-19 14:47:48 +0800

    Added Aggregation

---
+++ activity-aggregation.js
@@ -0,0 +1,236 @@
+/* 1 */
+{
+    "_id" : 312456,
+    "EmployeeName" : "Joongki Song",
+    "EmployeeAge" : 28,
+    "JobRole" : "Store Manager",
+    "Salary" : 120000
+}
+/* 2 */
+{
+    "_id" : 345342,
+    "EmployeeName" : "Joongkuk Jeon",
+    "EmployeeAge" : 25,
+    "JobRole" : "Store Associate",
+    "Salary" : 45000
+}
+/* 3 */
+{
+    "_id" : 334566,
+    "EmployeeName" : "Jin Kim",
+    "EmployeeAge" : 35,
+    "JobRole" : "Cashier",
+    "Salary" : 67500
+}
+/* 4 */
+{
+    "_id" : 245345,
+    "EmployeeName" : "Hyekyo Song",
+    "EmployeeAge" : 40,
+    "JobRole" : "Senior Cashier",
+    "Salary" : 72500
+}
+/* 5 */
+{
+    "_id" : 445364,
+    "EmployeeName" : "Tae Hyung Kim",
+    "EmployeeAge" : 22,
+    "JobRole" : "Store Associate",
+    "Salary" : 35000
+}
+
+/* 1 */
+{
+    "_id" : "LS0000123",
+    "Name" : "XYZ Chocolate Bar - 100g",
+    "Price" : 5.23,
+    "Quantity" : 25000,
+    "Category" : [ 
+        "chocolates", 
+        "sweets"
+    ]
+}
+
+/* 2 */
+{
+    "_id" : "LS0003123",
+    "Name" : "Milk Non-fat - 1lt",
+    "Price" : 3,
+    "Quantity" : 1000,
+    "Category" : [ 
+        "dairy", 
+        "healthy"
+    ]
+}
+
+/* 3 */
+{
+    "_id" : "LS0004566",
+    "Name" : "Eggs - 12 Pack",
+    "Price" : 6,
+    "Quantity" : 5000,
+    "Category" : [ 
+        "poultry", 
+        "generic"
+    ]
+}
+
+/* 4 */
+{
+    "_id" : "LS0008542",
+    "Name" : "Carrots (Packed) - 250g",
+    "Price" : 3.5,
+    "Quantity" : 3000,
+    "Category" : [ 
+        "vegetables", 
+        "healthy", 
+        "organic"
+    ]
+}
+
+/* 5 */
+{
+    "_id" : "LS0009846",
+    "Name" : "Beans (Packed) - 250g",
+    "Price" : 6.75,
+    "Quantity" : 6000,
+    "Category" : [ 
+        "vegetables", 
+        "healthy", 
+        "organic"
+    ]
+}
+
+/* 6 */
+{
+    "_id" : "LS0009100",
+    "Name" : "Bell Pepper (Packed) - 250 g",
+    "Price" : 4.95,
+    "Quantity" : 12000,
+    "Category" : [ 
+        "vegetables", 
+        "healthy", 
+        "organic"
+    ]
+}
+
+/* 7 */
+{
+    "_id" : "LS0002688",
+    "Name" : "ZZ Butter - 500 g",
+    "Price" : 25,
+    "Quantity" : 500,
+    "Category" : [ 
+        "dairy", 
+        "healthy", 
+        "premium"
+    ]
+}
+
+/* 1 */
+{
+    "_id" : "BL2021005",
+    "GrossAmount" : 105.65,
+    "Discounts" : 10,
+    "Net Amount" : 95.65,
+    "Date/Time" : ISODate("2021-01-01T16:00:00.000Z")
+}
+
+/* 2 */
+{
+    "_id" : "BL2021006",
+    "GrossAmount" : 45.25,
+    "Discounts" : 0,
+    "Net Amount" : 45.25,
+    "Date/Time" : ISODate("2021-01-01T16:15:55.000Z")
+}
+
+/* 3 */
+{
+    "_id" : "BL2021007",
+    "GrossAmount" : 153.33,
+    "Discounts" : 20.33,
+    "Net Amount" : 133,
+    "Date/Time" : ISODate("2021-01-01T16:31:08.000Z")
+}
+
+/* 4 */
+{
+    "_id" : "BL2021008",
+    "GrossAmount" : 21,
+    "Discounts" : 0,
+    "Net Amount" : 21,
+    "Date/Time" : ISODate("2021-01-01T20:25:52.000Z")
+}
+
+/* 5 */
+{
+    "_id" : "BL2021009",
+    "GrossAmount" : 33.5,
+    "Discounts" : 20.5,
+    "Net Amount" : 13,
+    "Date/Time" : ISODate("2021-01-01T08:45:12.000Z")
+}
+
+/* 6 */
+{
+    "_id" : "BL2021010",
+    "GrossAmount" : 105.65,
+    "Discounts" : 10,
+    "NetAmount" : 95.65,
+    "Date/Time" : ISODate("2021-01-01T11:02:35.000Z")
+}
+
+/* 1 */
+{
+    "_id" : "PROMO01",
+    "Name" : "Sales Promo",
+    "Period" : 7,
+    "Daily Sales" : [ 
+        20, 
+        50, 
+        12, 
+        30, 
+        45, 
+        15, 
+        60
+    ]
+}
+
+/* 2 */
+{
+    "_id" : "PROMO02",
+    "Name" : "Milk Promo",
+    "Period" : 2,
+    "Daily Sales" : [ 
+        120, 
+        200
+    ]
+}
+
+/* 3 */
+{
+    "_id" : "PROMO03",
+    "Name" : "Meat Promo",
+    "Period" : 3,
+    "Daily Sales" : [ 
+        101, 
+        205
+    ]
+}
+
+/* 4 */
+{
+    "_id" : "PROMO04",
+    "Name" : "Meat Promo",
+    "Period" : 3,
+    "Daily Sales" : [ 
+        68, 
+        88, 
+        105, 
+        188, 
+        74, 
+        278, 
+        350
+    ]
+}
